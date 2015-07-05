b96d3ba1cf9594e2ca5bbabe6f2b4a67a185b932
Manuel Limones <limonesu.me@gmail.com>
2015-07-05 03:30:24 -0500

    add mdk support for serial_stdio

--- retarget/gcc/source/serial_stdio.c
+++ retarget/gcc/source/serial_stdio.c
@@ -29,11 +29,18 @@ int serial_gets(Serial_t  serial,char * pBuffer, int bufferSize){
 	return bufferSize-1;
 }
 
+#define OUT_BUFFER_SIZE 80
+
 void serial_printf(Serial_t  serial, const char * format ,...){
 	char * tempBuffer;
 	va_list args;
 	va_start(args, format);
+	#if defined ( __GNUC__ )
 	vasprintf(&tempBuffer,format, args);
+	#else	
+	tempBuffer=malloc(OUT_BUFFER_SIZE);//alocate memory manualy
+	vsnprintf(tempBuffer,OUT_BUFFER_SIZE,format,args);
+	#endif
 	va_end(args);
 	serial_puts(serial,tempBuffer);
 	free(tempBuffer);
