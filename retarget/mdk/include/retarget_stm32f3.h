bd9462fc18f63dd3694077e9628a5cdd7f72afd0
Manuel Limones <limonesu.me@gmail.com>
2015-07-05 03:42:14 -0500

    remove unused mdk retarget and update gcc Makefile and keil proyect

--- retarget/mdk/include/retarget_stm32f3.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RETARGET_TM4C_H
-#define __RETARGET_TM4C_H
-#include "stm32f30x.h"                  // Device header
-#include "retarget.h"
-	// U0Rx (VCP receive) connected to PA2
-	// U0Tx (VCP transmit) connected to PA3
-	void UART2_init(void);
-#endif
